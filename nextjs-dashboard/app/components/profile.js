3801ef9fc7d8c8ee0a7c17c8d81e49d361e9650f
GsusMRosas <jmoreno91@alumnos.uaq.mx>
2024-11-11 18:41:04 -0600

    librerías bases de datos

--- nextjs-dashboard/app/components/profile.js
+++ nextjs-dashboard/app/components/profile.js
@@ -1,18 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppnameLi from "../components/appnameLi.js";
 
 function Profile() {
-    return(
+    const [userData, setUserData] = useState({ nombre: '', correo: '', telefono: '' });
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('/api/user');
+                const data = await response.json();
+                setUserData(data);
+            } catch (err) {
+                console.error('Error fetching user data:', err);
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
         <div>
-            <AppnameLi/>
+            <AppnameLi />
             <div className="bg-blue-50 flex">
-                <div className="bg-gray-200 p-20 mx-10 my-20 space-y-4">
+                <div className="bg-gray-200 p-20 mx-10 my-20 space-y-6 text-center">
                     <h1 className='pb-10 font-bungee text-3xl text-backgroundBlue'>Perfil</h1>
-                    <p>Nombre: </p>
-                    <p>Correo: </p>
-                    <p>Telefono: </p>
+                    <p>Nombre: {userData.nombre}</p>
+                    <p>Correo: {userData.correo}</p>
+                    <p>Telefono: {userData.telefono}</p>
                     <button type="submit" className="w-40 bg-white">Actualizar perfil</button>
                 </div>
                 <div className="bg-gray-200 px-20 py-20 my-20 mx-10">
@@ -20,13 +35,12 @@ function Profile() {
                         <h1 className="pb-10 font-bungee text-xl text-backgroundBlue">Pedido actual</h1>
                     </div>
                     <div>
-                        <h1 h1 className="pb-10 font-bungee text-xl text-backgroundBlue">Historial de pedidos</h1>
+                        <h1 className="pb-10 font-bungee text-xl text-backgroundBlue">Historial de pedidos</h1>
                     </div>
                 </div>
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default Profile;
